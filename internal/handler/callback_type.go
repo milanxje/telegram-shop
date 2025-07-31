c32bf7a91f388341a8f8ea30e504d30dff72a0a2
milanxje <reddit434@gmail.com>
2025-07-31 20:52:59 +0300

    Update callback_type.go

--- internal/handler/callback_type.go
+++ internal/handler/callback_type.go
@@ -1,12 +1,15 @@
 package handler
 
 const (
-	CallbackBuy           = "buy"
-	CallbackSell          = "sell"
-	CallbackStart         = "start"
-	CallbackConnect       = "connect"
-	CallbackPayment       = "payment"
-	CallbackTrial         = "trial"
-	CallbackActivateTrial = "activate_trial"
-	CallbackReferral      = "referral"
+    CallbackBuy         = "buy"
+    CallbackSell        = "sell"
+    CallbackStart       = "start"
+    CallbackConnect     = "connect"
+    CallbackPayment     = "payment"
+    CallbackTrial       = "trial"
+    CallbackActivateTrial = "activate_trial"
+    CallbackReferral    = "referral"
+    TextPricingInfo       = "pricing_info"
+    TextChoosePaymentType = "pricing_payment_type"
+    TextConfirmPayment    = "pricing_payment_confirm"
 )
